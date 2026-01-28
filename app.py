f91fe99891b0ad283c4c7d13e409abf5fcd35e5e
Ashit-10 <95882932+Ashit-10@users.noreply.github.com>
2026-01-28 21:48:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ for photo in photos:
         cap = None
         try:
             if "_roll_" in photo:
-                cap = photo.split("_roll_")[1]
+                cap = (photo.split("_roll_")[1]).replace(".jpg", "")
         except:
             cap = None
         print()
